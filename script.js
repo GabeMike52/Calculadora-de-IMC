1ffda04a00e0de57212242349e315d30d883463f
Gabriel Carneiro <gmiqueiascarneiro52@gmail.com>
2024-06-13 07:36:28 -0300

    /featAdicionadas condicionais para melhor funcionamento do código.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function calcularIMC() {
         let peso = document.getElementById("peso").value;
         let altura = document.getElementById("altura").value;
+        if (peso != Number) {
+            alert("Por favor, insira um número válido para o peso.");
+            return;
+        } else if (altura != Number) {
+            alert("Por favor, insira um número válido para a altura.");
+            return;
+        } else {
         let imc = peso / (altura * altura);
         alert(imc);
         return imc
+      }     
     }
