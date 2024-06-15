a9abbf83e0141f42106c16c8ea44240fcaf4329a
Gabriel Carneiro <gmiqueiascarneiro52@gmail.com>
2024-06-15 06:21:44 -0300

    feat/Adicionadas condicionais para alertar o usuário acerca do resultado de forma dinâmica

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function calcularIMC() {
         let peso = document.getElementById("peso").value;
         let altura = document.getElementById("altura").value;
-        if (peso != Number) {
-            alert("Por favor, insira um número válido para o peso.");
-            return;
-        } else if (altura != Number) {
-            alert("Por favor, insira um número válido para a altura.");
-            return;
-        } else {
         let imc = peso / (altura * altura);
-        alert(imc);
+        if (imc <= 16.9) 
+          alert(imc + " " + ":"+ " " + "Magreza severa");
+        else if (imc > 16.9 && imc <= 18.4)
+          alert(imc + " " + ":"+ " " + "Abaixo do peso");
+        else if (imc > 18.4 && imc <= 24.9)
+          alert(imc + " " + ":"+ " " + "Peso normal");
+        else if (imc > 24.9 && imc <= 29.9)
+          alert(imc + " " + ":"+ " " + "Acima do peso");
+        else if (imc > 29.9 && imc <= 34.9)
+          alert(imc + " " + ":"+ " " + "Obesidade Grau I")
+        else if (imc > 34.9 && imc <= 40)
+          alert(imc + " " + ":"+ " " + "Obesidade Grau II")
+        else if (imc > 40)
+          alert(imc + " " + ":"+ " " + "Obesidade Grau III")
         return imc
       }     
-    }
